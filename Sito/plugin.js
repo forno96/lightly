9ecaa2bb1e91207456e8c2ced193b3ebd6714393
Francesco Fornari <forno96@gmail.com>
2020-02-17 21:50:51 +0100

    Introducing class

--- Sito/plugin.js
+++ Sito/plugin.js
@@ -1,18 +1,73 @@
-var edit = 0;
-var stack = [];
+function sanitizeID(value){
+  var tmp = value.toFixed().length;
+  var ret = '';
 
-function insStack(op, pos,  content, by){
-  stack.unshift({
-    "id": "edit-" + edit,
-    "op": op,
-    "pos": pos,
-    "content": content,
-    "by": by,
-    "timestamp": 0
-    
-  });
-  edit++;
+  for (var i = 5; i > tmp; i--) {ret += '0';}
+
+  return ret + value;
 }
+
+function getTime(){ return (new Date().getTime());}
+
+class Mechanical {
+  constructor(){
+    this.editMech = 0;
+    this.stackMech = [];
+  }
+
+  insStack(op, pos, content, by){
+    this.stackMech.unshift({
+      "id": "mech-" + sanitizeID(this.editMech),
+      "op": op,
+      "pos": pos,
+      "content": content,
+      "by": by,
+      "timestamp": getTime(),
+    });
+
+    this.editMech++;
+  }
+
+  get retStack(){ return(this.stackMech);}
+
+  resetStack(){this.stackMech = [];}
+}
+
+var mech = new Mechanical();
+
+/*
+NOOP, WRAP/UNWRAP, JOIN/SPLIT, REPLACE, INSERT/DELETE,
+PUNTUATION, WORDREPLACE, WORDCHANGE, TEXTREPLACE
+*/
+class Structular {
+  constructor(){
+    this.stackStruct = [];
+    this.editStruct = 0;
+  }
+
+  insStack(op, by){
+    /*mech.insStack("DEL",23,"ciao","Francesco");
+    mech.insStack("DEL",23,"ciao","Francesco");*/
+
+    var item = {
+      "id": "struct-" + sanitizeID(this.editStruct),
+      "op": op,
+      "by": by,
+      "timestamp": getTime(),
+      "items": []
+    }
+
+    item.items = mech.retStack[0]
+
+    this.stackStruct.unshift(item);
+
+    this.editStruct++;
+  }
+
+  get retStack(){ return(this.stackStruct);}
+}
+
+var struct = new Structular();
 
 tinymce.PluginManager.add('example', function(editor, url) {
   var openDialog = function () {
@@ -58,18 +113,18 @@ tinymce.PluginManager.add('example', function(editor, url) {
   });
 
   // Adds a menu item, which can then be included in any menu via the menu/menubar configuration
-  editor.ui.registry.addMenuItem('example', {
+  /*editor.ui.registry.addMenuItem('example', {
     text: 'Example plugin',
     onAction: function() {
       // Open window
       openDialog();
     }
-  });
+  });*/
 
   return {
     getMetadata: function () {
       return  {
-        name: "Example plugin",
+        name: "Undo stack plugin",
         url: "http://exampleplugindocsurl.com"
       };
     }
