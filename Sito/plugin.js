6961efc11de01ec95be353ae7a2ac194d47d9da8
Francesco Fornari <f.fornari@vargroup.it>
2020-07-14 18:37:58 +0200

    PATCH: Cleaning code and better check

--- Sito/plugin.js
+++ Sito/plugin.js
@@ -50,73 +50,6 @@ class Mechanical {
   emptyRevertedMech() {this.revertedMech = [];}
 }
 
-/*
-NOOP, WRAP/UNWRAP, JOIN/SPLIT, REPLACE, INSERT/DELETE,
-PUNTUATION, WORDREPLACE, WORDCHANGE, TEXTREPLACE
-*/
-class Structular {
-  constructor(){
-    this.stackStruct = [];
-    this.editStruct = 0;
-  }
-
-  insItem(op, by, mech, listMech, old, nw){
-    var item = {
-      "id": "struct-" + sanitizeID(this.editStruct),
-      "op": op,
-      "old": old,
-      "new": nw,
-      "by": by,
-      "timestamp": getTime(),
-      "items": []
-    };
-
-    listMech.forEach((i) => {
-      item.items.push(mech.retItem(i));
-    });
-
-    this.stackStruct.push(item);
-
-    this.editStruct++;
-    return (this.editStruct -1);
-  }
-
-  get stack(){ return(this.stackStruct);}
-  retItem(i) {return(this.stackStruct[i]);}
-}
-
-/*
-MEANING, FIX, STYLE, EDITCHAIN, EDITWAR, EDITWAKE
-*/
-class Semantic {
-  constructor(){
-    this.stackSem = [];
-    this.editSem = 0;
-  }
-
-  insItem(op, by, struct, listStruct, old, nw){
-
-    var item = {
-      "id": "sem-" + sanitizeID(this.editSem),
-      "op": op,
-      "old": old,
-      "new": nw,
-      "items": []
-    };
-
-    listStruct.forEach((i) => {
-      item.items.push(struct.retItem(i));
-    });
-
-    this.stackSem.push(item);
-
-    this.editSem++;
-    return (this.editSem -1)
-  }
-
-  get stack(){ return(this.stackSem);}
-}
-
 /* ----- */
 // INIT CLASS and VAR
 oldState = undefined;
@@ -138,30 +71,11 @@ async function checkChange(){
     catch(err) { await delay(200); } // Per dare il tempo a tinyMCE di caricarsi, err sta perchè è supportato solo da ES10
   }
 
-  let launch = true;
 
   catchChange(); // Per caricare lo stato
   ["keyup", "click", "onclick"].forEach((event, i) => {
-    document.addEventListener(event, (evn) => { rightKey(evn); launch = true; });
-    editor.addEventListener  (event, (evn) => { rightKey(evn); launch = true; });
-  });
-
-  editor.addEventListener("keydown", function () { launch = false; });
-
-  while (true) {
-    await delay (1000);
-    if (launch == true) catchChange();
-  }
-}
-
-async function rightKey(event) {
-  //console.log(event);
-
-  if (event.key == undefined) { catchChange(); return (true); }
-
-  console.log(`Keyup: "${event.key}"`);
-  [" ", ".", ",", ";", "Enter", "Backspace"].forEach((key, i) => {
-    if (key == event.key) catchChange();
+    document.addEventListener(event, (evn) => { catchChange(); });
+    editor.addEventListener  (event, (evn) => { catchChange(); });
   });
 }
 
@@ -175,10 +89,13 @@ function catchChange(){ // E' da far cercare il cambiamento solo all'interno di 
   if (oldState == undefined) {
     oldState = newState;
     console.log('State Loaded');
-    return (false);
+    return;
   }
 
-  if (oldState == newState) { console.log('State Unchanged'); return (true);}
+  if (oldState == newState) {
+    console.log('State Unchanged');
+    return;
+  }
 
   var start = 0;
   var newEnd = newState.length -1;
@@ -207,7 +124,7 @@ function undoChange() {
 
   if (mech.stack.length == 0) { // Se la pila è vuota undoChange non deve fare nulla
     console.log("Undo stack is empty");
-    return (false);
+    return;
   }
 
   state = catchState();
@@ -340,32 +257,13 @@ tinymce.PluginManager.add('UndoStack', function(editor, url) {
   });
   editor.shortcuts.add('ctrl+shift+z', "Redo shortcut", function() { redoChange(); });
 
+  editor.on('ExecCommand', function() { catchChange(); });
 
   return {
     getMetadata: function () {
-      return  {
-        name: "Undo stack plugin",
-        url: "http://exampleplugindocsurl.com"
-      };
+      return  { name: "Undo stack plugin" };
     }
   };
 });
 
 checkChange();
-
-function test(){
-  var by = "Francesco";
-
-  var mech = new Mechanical();
-  var struct = new Structular();
-  var sem = new Semantic();
-
-  mech.insItem("DEL", 2343, "nuovo", by);
-  mech.insItem("DEL", 446, "</p><p>", by);
-
-  struct.insItem("NOOP", by, mech, [0,1]);
-
-  sem.insItem("MEANING", by, struct, [0]);
-
-  return sem.stack;
-}
